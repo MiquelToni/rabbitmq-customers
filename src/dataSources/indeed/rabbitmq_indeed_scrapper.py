b43350cc37ab012e42a79931f6193bf074939d27
malc <malc@trifork.com>
2023-02-20 10:23:04 +0100

    add indeed scrapper

--- src/dataSources/indeed/rabbitmq_indeed_scrapper.py
+++ src/dataSources/indeed/rabbitmq_indeed_scrapper.py
@@ -3,7 +3,31 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import json
-from ...domain.constants.iso_country_codes import ISO_COUNTRY_CODES
+
+# https://gist.github.com/jonbruner/64fd4774396448a0a96ee2ac396bff20
+ISO_COUNTRY_CODES = ["AF", "AX", "AL", "DZ", "AS", "AD", "AO", "AI", "AQ", "AG", "AR",
+                     "AM", "AW", "AU", "AT", "AZ", "BS", "BH", "BD", "BB", "BY", "BE",
+                     "BZ", "BJ", "BM", "BT", "BO", "BQ", "BA", "BW", "BV", "BR", "IO",
+                     "BN", "BG", "BF", "BI", "CV", "KH", "CM", "CA", "KY", "CF", "TD",
+                     "CL", "CN", "CX", "CC", "CO", "KM", "CG", "CD", "CK", "CR", "CI",
+                     "HR", "CU", "CW", "CY", "CZ", "DK", "DJ", "DM", "DO", "EC", "EG",
+                     "SV", "GQ", "ER", "EE", "ET", "FK", "FO", "FJ", "FI", "FR", "GF",
+                     "PF", "TF", "GA", "GM", "GE", "DE", "GH", "GI", "GR", "GL", "GD",
+                     "GP", "GU", "GT", "GG", "GN", "GW", "GY", "HT", "HM", "VA", "HN",
+                     "HK", "HU", "IS", "IN", "ID", "IR", "IQ", "IE", "IM", "IL", "IT",
+                     "JM", "JP", "JE", "JO", "KZ", "KE", "KI", "KP", "KR", "KW", "KG",
+                     "LA", "LV", "LB", "LS", "LR", "LY", "LI", "LT", "LU", "MO", "MK",
+                     "MG", "MW", "MY", "MV", "ML", "MT", "MH", "MQ", "MR", "MU", "YT",
+                     "MX", "FM", "MD", "MC", "MN", "ME", "MS", "MA", "MZ", "MM", "NA",
+                     "NR", "NP", "NL", "NC", "NZ", "NI", "NE", "NG", "NU", "NF", "MP",
+                     "NO", "OM", "PK", "PW", "PS", "PA", "PG", "PY", "PE", "PH", "PN",
+                     "PL", "PT", "PR", "QA", "RE", "RO", "RU", "RW", "BL", "SH", "KN",
+                     "LC", "MF", "PM", "VC", "WS", "SM", "ST", "SA", "SN", "RS", "SC",
+                     "SL", "SG", "SX", "SK", "SI", "SB", "SO", "ZA", "GS", "SS", "ES",
+                     "LK", "SD", "SR", "SJ", "SZ", "SE", "CH", "SY", "TW", "TJ", "TZ",
+                     "TH", "TL", "TG", "TK", "TO", "TT", "TN", "TR", "TM", "TC", "TV",
+                     "UG", "UA", "AE", "GB", "US", "UM", "UY", "UZ", "VU", "VE", "VN",
+                     "VG", "VI", "WF", "EH", "YE", "ZM", "ZW"]
 
 
 def save_found_offers(found_offers):
@@ -35,15 +59,14 @@ def maybe_close_popup(driver: webdriver.Chrome):
 
 
 def init_driver():
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    driver = webdriver.Chrome(options=options)
+    # options = webdriver.ChromeOptions()
+    # options.add_argument("--headless")
+    # driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome()
     return driver
 
 
 def get_job_offers_from_page(driver: webdriver.Chrome):
-    driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
-
     data = driver.execute_script(
         'return window.mosaic.providerData["mosaic-provider-jobcards"]')
 
@@ -56,25 +79,28 @@ def crawl_page():
 
     for code in ISO_COUNTRY_CODES:
         url = f'https://{code}.indeed.com/jobs?q=rabbitmq'
-        print("visiting", url)
         try:
             driver.get(url)
+            try:
+                print("visiting", url)
+                time.sleep(1.5)
 
-            driver.implicitly_wait(1)
-            reject_cookies_btn = driver.find_element(
-                By.ID, 'onetrust-reject-all-handler')
-            reject_cookies_btn.click()
+                get_job_offers_from_page(driver)
+                while goto_next_page(driver):
+                    time.sleep(1)
 
-            get_job_offers_from_page(driver)
-            while goto_next_page(driver):
-                maybe_close_popup(driver)
-                offers = offers + get_job_offers_from_page(driver)
-                break
-            print("Finished visiting", url)
-        except:
+                    maybe_close_popup(driver)
+                    new_offers = get_job_offers_from_page(driver)
+                    print("found", len(new_offers), "new offers")
+                    offers = offers + new_offers
+
+                print("Finished visiting", url)
+            except Exception as e:
+                print(e)
+        except Exception as e:
             print("Not Found", url)
+
     save_found_offers(offers)
 
 
 crawl_page()
-time.sleep(100)
